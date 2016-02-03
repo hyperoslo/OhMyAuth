a7761ba9bbe362d3b46e118303fe86b1a0e37dc2
Vadym Markov <markov.vadym@gmail.com>
2016-02-03 13:43:02 +0100

    Add token provider class

--- Sources/iOS/TokenProvider.swift
+++ Sources/iOS/TokenProvider.swift
@@ -1,9 +1,5 @@
-//
-//  TokenProvider.swift
-//  AzureOAuth
-//
-//  Created by Vadym Markov on 03/02/16.
-//  Copyright © 2016 Hyper Interaktiv AS. All rights reserved.
-//
-
 import Foundation
+
+@objc class TokenProvider: NSObject {
+
+}
