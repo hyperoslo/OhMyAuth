3be7420cf386b50cc35edb902023052809c2d5cf
Vadym Markov <markov.vadym@gmail.com>
2016-02-03 23:43:28 +0100

    Move config to shared

--- Sources/Shared/Config.swift
+++ Sources/Shared/Config.swift
@@ -1,9 +1,32 @@
-//
-//  Config.swift
-//  AzureOAuth
-//
-//  Created by Vadym Markov on 03/02/16.
-//  Copyright © 2016 Hyper Interaktiv AS. All rights reserved.
-//
-
 import Foundation
+
+@objc public class LoginConfig: NSObject {
+
+  public var loginURL: NSURL
+  public var changeUserURL: NSURL?
+  public let redirectURI: String
+
+  public init(loginURL: NSURL, redirectURI: String, changeUserURL: NSURL? = nil) {
+    self.loginURL = loginURL
+    self.changeUserURL = changeUserURL
+    self.redirectURI = redirectURI
+  }
+}
+
+@objc public class TokenConfig: NSObject {
+
+  public let accessGrantType = "authorization_code"
+  public let refreshGrantType = "refresh_token"
+
+  public var URL: NSURL
+  public var resource: String
+  public let clientId: String
+  public let clientSecret: String
+
+  public init(URL: NSURL, resource: String, clientId: String, clientSecret: String) {
+    self.URL = URL
+    self.resource = resource
+    self.clientId = clientId
+    self.clientSecret = clientSecret
+  }
+}
