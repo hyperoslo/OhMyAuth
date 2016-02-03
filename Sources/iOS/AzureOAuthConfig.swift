adb28fe9ab2379742c11984451d22bd9d5507120
Vadym Markov <markov.vadym@gmail.com>
2016-02-03 12:36:00 +0100

    Add Config

--- Sources/iOS/AzureOAuthConfig.swift
+++ Sources/iOS/AzureOAuthConfig.swift
@@ -1,9 +1,32 @@
-//
-//  AzureOAuthConfig.swift
-//  AzureOAuth
-//
-//  Created by Vadym Markov on 03/02/16.
-//  Copyright © 2016 Hyper Interaktiv AS. All rights reserved.
-//
-
 import Foundation
+
+public struct LoginConfig {
+
+  public var loginURL: NSURL
+  public var changeUserURL: NSURL?
+
+  public init(loginURL: NSURL, changeUserURL: NSURL?) {
+    self.loginURL = loginURL
+    self.changeUserURL = changeUserURL
+  }
+}
+
+public struct TokenConfig {
+
+  public let accessGrantType = "authorization_code"
+  public let refreshGrantType = "refresh_token"
+
+  public var URL: NSURL
+  public var resource: String
+  public let clientId: String
+  public let clientSecret: String
+  public let redirectURI: String
+
+  public init(URL: NSURL, resource: String, clientId: String, clientSecret: String, redirectURI: String) {
+    self.URL = URL
+    self.resource = resource
+    self.clientId = clientId
+    self.clientSecret = clientSecret
+    self.redirectURI = redirectURI
+  }
+}
