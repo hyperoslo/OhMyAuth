ae1595075c6b5e31941b7e3f2e9b9c215668fc27
Vadym Markov <markov.vadym@gmail.com>
2016-02-03 23:43:39 +0100

    Move error

--- Sources/Shared/Error.swift
+++ Sources/Shared/Error.swift
@@ -1,9 +1,12 @@
-//
-//  Error.swift
-//  AzureOAuth
-//
-//  Created by Vadym Markov on 03/02/16.
-//  Copyright © 2016 Hyper Interaktiv AS. All rights reserved.
-//
-
 import Foundation
+
+@objc enum Error: Int, ErrorType {
+  case InvalidRedirectURI = -1
+  case CodeParameterNotFound = -2
+  case ExpirationDateNotFound = -3
+  case NoConfigFound = -4
+  case NoDataInResponse = -5
+  case AuthenticationFailed = -6
+  case NoAccessTokenFound = -7
+  case NoRefreshTokenFound = -8
+}
