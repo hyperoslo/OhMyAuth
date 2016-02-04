e5b1496773d3c85be2bfc190d906008b5ea319d7
Vadym Markov <markov.vadym@gmail.com>
2016-02-04 19:29:30 +0100

    Introduce auth service

--- Sources/Shared/AuthService.swift
+++ Sources/Shared/AuthService.swift
@@ -1,9 +1,18 @@
-//
-//  AuthService.swift
-//  AzureOAuth
-//
-//  Created by Vadym Markov on 04/02/16.
-//  Copyright © 2016 Hyper Interaktiv AS. All rights reserved.
-//
-
 import Foundation
+
+@objc public class AuthService: NSObject {
+
+  public let name: String
+  public let tokenProvider: TokenProvider
+  public let codeProvider: CodePrivider
+  public var locker: Lockable
+
+  // MARK: - Initialization
+
+  public init(name: String, config: AuthConfig) {
+    self.name = name
+    locker = Locker(name: name)
+    tokenProvider = TokenProvider(config: config, locker: locker)
+    codeProvider = CodePrivider(config: config, locker: locker, tokenProvider: tokenProvider)
+  }
+}
