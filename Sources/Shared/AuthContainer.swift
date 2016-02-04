b017127731856666da4b7f35566cb943b9bb94b0
Vadym Markov <markov.vadym@gmail.com>
2016-02-04 19:27:38 +0100

    Change auth container

--- Sources/Shared/AuthContainer.swift
+++ Sources/Shared/AuthContainer.swift
@@ -2,7 +2,13 @@ import Foundation
 
 @objc public class AuthContainer: NSObject {
 
-  public static var locker: Lockable = Locker()
-  public static var authenticator: Authenticator = Authenticator()
-  public static var tokenProvider: TokenProvider = TokenProvider()
+  static var services = [String: AuthService]()
+
+  public static func addService(service: AuthService) {
+    services[service.name] = service
+  }
+
+  public static func resolveService(name: String) -> AuthService? {
+    return services[name]
+  }
 }
