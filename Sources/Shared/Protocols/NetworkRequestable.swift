bd250d6f6ca6a4a80e0a81d5a056bcc578d0f265
Vadym Markov <markov.vadym@gmail.com>
2016-03-02 17:06:39 +0100

    Handle errors in the request

--- Sources/Shared/Protocols/NetworkRequestable.swift
+++ Sources/Shared/Protocols/NetworkRequestable.swift
@@ -1,4 +1,5 @@
 import Alamofire
+import Sugar
 
 protocol NetworkRequestable {
   var URL: NSURL { get }
@@ -8,11 +9,27 @@ protocol NetworkRequestable {
 extension NetworkRequestable {
 
   func start(completion: (result: Result<AnyObject>) -> Void) {
-    Alamofire.request(.POST, URL, parameters: parameters, encoding: .URL).responseJSON {
-      response in
-
+    Alamofire.request(.POST, URL, parameters: parameters, encoding: .URL).responseJSON { response in
       guard response.result.isSuccess else {
         completion(result: .Failure(response.result.error))
+        return
+      }
+
+      guard let value = response.result.value else {
+        completion(result: .Failure(Error.NoDataInResponse.toNSError()))
+        return
+      }
+
+      if let JSON = value as? JSONDictionary, errorDictionary = JSON["error"] as? JSONDictionary {
+        let error = Error.TokenRequestFailed.toNSError(
+          JSON["error_description"] as? String, userInfo: errorDictionary)
+
+        completion(result: .Failure(error))
+        return
+      }
+
+      guard response.response?.statusCode == 200 else {
+        completion(result: .Failure(Error.TokenRequestFailed.toNSError()))
         return
       }
 
