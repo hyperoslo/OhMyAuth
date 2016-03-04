7928db598ab51e0893211ca2e6c50eda65cff22e
Christoffer Winterkvist <christoffer@hyper.no>
2016-03-04 16:08:28 +0100

    Make Keys internal

--- Sources/Shared/KeychainLocker.swift
+++ Sources/Shared/KeychainLocker.swift
@@ -2,15 +2,15 @@ import Foundation
 import Keychain
 import Sugar
 
-public struct Keys {
+struct Keys {
 
-  public static let service = "\(Application.name)"
-  public static let accessToken = "\(Application.name)-AccessToken"
-  public static let refreshToken = "\(Application.name)-RefreshToken"
-  public static let tokenType = "\(Application.name)-TokenType"
-  public static let expiryDate = "\(Application.name)-ExpiryDate"
-  public static let userName = "\(Application.name)-UserName"
-  public static let userUPN = "\(Application.name)-UserUPN"
+  static let service = "\(Application.name)"
+  static let accessToken = "\(Application.name)-AccessToken"
+  static let refreshToken = "\(Application.name)-RefreshToken"
+  static let tokenType = "\(Application.name)-TokenType"
+  static let expiryDate = "\(Application.name)-ExpiryDate"
+  static let userName = "\(Application.name)-UserName"
+  static let userUPN = "\(Application.name)-UserUPN"
 }
 
 @objc public class KeychainLocker: UserDefaultsLocker {
