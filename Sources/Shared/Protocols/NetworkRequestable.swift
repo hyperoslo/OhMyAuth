5792a0a827bd87aa7de17391ae4af8774b6059e3
Christoffer Winterkvist <christoffer@hyper.no>
2016-08-12 14:01:23 +0200

    Use manager instead of sharedInstance

--- Sources/Shared/Protocols/NetworkRequestable.swift
+++ Sources/Shared/Protocols/NetworkRequestable.swift
@@ -11,7 +11,7 @@ protocol NetworkRequestable {
 extension NetworkRequestable {
 
   func start(completion: (result: Result<AnyObject>) -> Void) {
-    Alamofire.request(.POST, URL, parameters: parameters, encoding: .URL, headers: headers).responseJSON { response in
+    manager.request(.POST, URL, parameters: parameters, encoding: .URL, headers: headers).responseJSON { response in
       guard response.result.isSuccess else {
         completion(result: .Failure(response.result.error))
         return
