cf11a29ba9ad06faf269e93cf52837db2a47aa24
Christoffer Winterkvist <christoffer@hyper.no>
2016-10-28 15:56:06 +0200

    Use OperationQueue.main

--- Sources/Shared/Networking/Networking.swift
+++ Sources/Shared/Networking/Networking.swift
@@ -4,7 +4,7 @@ open class Networking {
   public let session: URLSession
   
   public init(configuration: URLSessionConfiguration) {
-    session = URLSession(configuration: configuration)
+    session = URLSession(configuration: configuration, delegateQueue: OperationQueue.main)
   }
   
   public func post(url: URL, parameters: [String: Any], headers: [String: String], completion: @escaping ((Data?, URLResponse?, Error?) -> Void)) {
