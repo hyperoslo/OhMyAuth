bb7edf1fd01d23403d95943d1c7267adf4920a99
Khoa Pham <onmyway133@gmail.com>
2016-11-30 13:08:00 +0100

    Remove the mention of Application.name as it is mentioned by the service property

--- Sources/Shared/Locker/Keys.swift
+++ Sources/Shared/Locker/Keys.swift
@@ -2,10 +2,10 @@ import Foundation
 
 public struct Keys {
 
-  public static let accessToken = "\(Application.name)-AccessToken"
-  public static let refreshToken = "\(Application.name)-RefreshToken"
-  public static let tokenType = "\(Application.name)-TokenType"
-  public static let expiryDate = "\(Application.name)-ExpiryDate"
-  public static let userName = "\(Application.name)-UserName"
-  public static let userUPN = "\(Application.name)-UserUPN"
+  public static let accessToken = "AccessToken"
+  public static let refreshToken = "RefreshToken"
+  public static let tokenType = "TokenType"
+  public static let expiryDate = "ExpiryDate"
+  public static let userName = "UserName"
+  public static let userUPN = "UserUPN"
 }
