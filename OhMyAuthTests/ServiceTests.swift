ff0fcceb0ca97d5b01fe2077958c0c03ec005acc
Khoa Pham <onmyway133@gmail.com>
2017-03-13 11:37:21 +0100

    Add ServiceTests

---
+++ OhMyAuthTests/ServiceTests.swift
@@ -0,0 +1,57 @@
+import XCTest
+@testable import OhMyAuth
+
+class ServiceTests: XCTestCase {
+
+  class LockerMock: Lockable {
+    var accessToken: String?
+    var refreshToken: String?
+    var tokenType: String?
+    var expiryDate: Date?
+    var userName: String?
+    var userUPN: String?
+
+    required init(name: String) {
+
+    }
+
+    func clear() {
+
+    }
+
+    @discardableResult func synchronize() -> Bool {
+      return false
+    }
+  }
+
+  var locker: LockerMock!
+  var config: AuthConfig!
+  var service: AuthService!
+
+  override func setUp() {
+    super.setUp()
+
+    locker = LockerMock(name: "locker")
+    config = AuthConfig(clientId: "", accessTokenUrl: URL(string: "http://testservice.no")!)
+    config.minimumValidity = 1
+    service = AuthService(name: "service", config: config, locker: locker)
+  }
+
+  func testBeforeExpiredDate() {
+    locker.expiryDate = Date(timeIntervalSinceNow: 1)
+    XCTAssertTrue(service.tokenIsExpired)
+
+    locker.expiryDate = Date(timeIntervalSinceNow: 2)
+    XCTAssertFalse(service.tokenIsExpired)
+  }
+
+  func testExactExpiredDate() {
+    locker.expiryDate = Date()
+    XCTAssertTrue(service.tokenIsExpired)
+  }
+
+  func testAfterExpiredDate() {
+    locker.expiryDate = Date(timeIntervalSinceNow: -1)
+    XCTAssertTrue(service.tokenIsExpired)
+  }
+}
